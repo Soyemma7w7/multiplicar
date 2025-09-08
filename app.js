237bbdaec63ad110bb9f52b1f4439e65851bc3e1
Soyemma7w7 <emmamnuel61031@gmail.com>
2025-09-08 11:40:36 -0500

    multiplicarapi

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const cors = require('cors'); 
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.post('/multiplicar', (req, res) => {
+    const { a, b } = req.body;
+    const resultado = Number(a) * Number(b);
+    res.json({ resultado });
+});
+
+app.listen(3000, () => {
+    console.log('API ejecutandose en http://localhost:3000');
+});
